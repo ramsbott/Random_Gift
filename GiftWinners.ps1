74d169cde8b725870c2a754dafa725b1db4e281f
ramsbott <47227850+ramsbott@users.noreply.github.com>
2020-12-18 08:31:43 -0600

    Add files via upload

---
+++ GiftWinners.ps1
@@ -0,0 +1,59 @@
+﻿##Holiday Gift Give Away##
+$List = Get-Content -Path ./UserList.txt
+$Prizes = Get-Content -Path ./Prize.txt
+$Employees = [System.Collections.ArrayList] ($List)
+
+foreach ($Prize in $Prizes) {
+    $Employee = Get-Random -InputObject $Employees
+    Write-Host "
+########## PRIZE ##########
+
+          $Prize
+
+##########################"
+    sleep 2
+    Write-Host ""
+    Write-Host "The winner is ......"
+    sleep 1
+    Write-Host""
+    Write-Host "$Employee" -ForegroundColor Green
+    Write-Host ""
+    Write-Host "Congrats to $Employee on your new $Prize"
+    Write-Host ""
+    echo "$Employee; $Prize" >> Winners.txt
+    $Employees.Remove($Employee)
+    Read-Host "Press ENTER to Continue"
+    Write-Host "
+    ----------------------------------------------------------------------------------------------------
+    "
+    }
+
+Write-Host "
+             /\
+            <  >
+             \/
+             /\
+            /  \
+           /++++\
+          /  ()  \
+          /      \
+         /~`~`~`~`\
+        /  ()  ()  \
+        /          \
+       /*&*&*&*&*&*&\
+      /  ()  ()  ()  \
+      /              \
+     /++++++++++++++++\
+    /  ()  ()  ()  ()  \
+    /                  \
+   /~`~`~`~`~`~`~`~`~`~`\
+  /  ()  ()  ()  ()  ()  \
+  /*&*&*&*&*&*&*&*&*&*&*&\
+ /                        \
+/,.,.,.,.,.,.,.,.,.,.,.,.,.\
+           |   |
+          |`````|
+          \_____/
+ End of event. Happy Holidays
+"
+sleep 5
